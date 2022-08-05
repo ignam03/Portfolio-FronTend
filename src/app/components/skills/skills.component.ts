01c2ff61c0e29a62816b1a20853031098b514dd0
ignam03 <ignafree97@gmail.com>
2022-08-04 22:13:28 -0300

    skills conexion con el backend

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,3 +1,5 @@
+import { SkillService } from './../../services/skill/skill.service';
+import { Habilidad } from './../../models/habilidad/habilidad';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillsComponent implements OnInit {
 
-  constructor() { }
+  habilidad!: Habilidad;
+  habilidades!: Array<Habilidad>;
+
+  constructor(private habilidadSvc: SkillService) {
+
+  }
 
   ngOnInit(): void {
+    this.cargarSkills();
+  }
+
+  cargarSkills() {
+    this.habilidadSvc.getSkills().subscribe(result => {
+      console.log(result);
+      this.habilidades = new Array<Habilidad>();
+      result.forEach((element: any) => {
+        this.habilidad = new Habilidad();
+        Object.assign(this.habilidad, element);
+        this.habilidades.push(this.habilidad);
+        console.log(this.habilidad);
+      })
+    })
   }
 
 }
--- src/app/services/skill/skill.service.ts
+++ src/app/services/skill/skill.service.ts
@@ -1,3 +1,5 @@
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +7,21 @@ import { Injectable } from '@angular/core';
 })
 export class SkillService {
 
-  constructor() { }
+  url: string = "http://localhost:8080/skills/ver/todas"
+
+  constructor(private _http: HttpClient) {
+
+  }
+
+  getSkills(): Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+
+      }),
+      params: new HttpParams({
+
+      })
+    }
+    return this._http.get(this.url);
+  }
 }
