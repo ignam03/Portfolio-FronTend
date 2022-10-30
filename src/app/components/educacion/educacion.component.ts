a68b17520d6a32e6d78f77a564b084adfd0d90a4
Ignacio Madrigal <cmadrigal@mobydigital.com>
2022-10-29 22:49:08 -0300

    add methods update & delete & create

--- src/app/components/educacion/educacion.component.ts
+++ src/app/components/educacion/educacion.component.ts
@@ -1,6 +1,10 @@
+import { TokenService } from './../../services/token/token.service';
+import { Experiencia } from './../../models/experiencia/experiencia';
+import { ExperienciaService } from './../../services/exp/experiencia.service';
 import { EducacionService } from './../../services/edu/educacion.service';
 import { Educacion } from './../../models/educacion/educacion';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-educacion',
@@ -9,29 +13,88 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EducacionComponent implements OnInit {
 
+  eduLoad!: Educacion;
   educacion!: Educacion;
   educacionList!: Array<Educacion>;
+  experience!: Experiencia;
+  experienceList!: Array<Experiencia>;
+  isLogged = false;
 
-  constructor(private educacionSvc:EducacionService) { 
-
+  constructor(private educacionSvc: EducacionService, private experienceSvc: ExperienciaService, private activatedRoute: ActivatedRoute, private tokenService: TokenService) {
+    this.educacion = new Educacion();
   }
 
   ngOnInit(): void {
     this.cargarEducacionList();
-
+    this.loadAllExp();
+    this.activatedRoute.params.subscribe((params: any) => {
+      this.loadEducation(params['id'])
+    })
+    if (this.tokenService.getToken()) {
+      this.isLogged = true;
+    } else {
+      this.isLogged = false;
     }
+  }
 
 
-  cargarEducacionList(){
+  cargarEducacionList() {
     this.educacionSvc.getEduTodas().subscribe(result => {
       console.log(result);
       this.educacionList = new Array<Educacion>();
       result.forEach((element: any) => {
         this.educacion = new Educacion();
-        Object.assign(this.educacion,element);
+        Object.assign(this.educacion, element);
         this.educacionList.push(this.educacion);
         console.log(this.educacion)
       })
+    })
+  }
+
+  loadAllExp() {
+    this.experienceSvc.getAllExperiences().subscribe(result => {
+      console.log(result);
+      this.experienceList = new Array<Experiencia>();
+      result.forEach((element: any) => {
+        this.experience = new Experiencia();
+        Object.assign(this.experience, element);
+        this.experienceList.push(this.experience);
+        console.log(this.experienceList);
+      })
+    })
+  }
+
+  loadEducation(id: number) {
+    this.eduLoad = new Educacion();
+    this.educacionSvc.getEducation(id).subscribe(result => {
+      console.log(result);
+      Object.assign(this.eduLoad, result);
+      console.log(this.eduLoad)
+    })
+  }
+
+  updateEducation() {
+    console.log(this.eduLoad);
+    this.educacionSvc.updateEducation(this.eduLoad).subscribe(result => {
+      console.log("actualizado creo");
+      window.location.reload();
+    })
+  }
+
+  createdEducation() {
+    console.log(this.eduLoad);
+    this.educacionSvc.createdEducation(this.eduLoad).subscribe(result => {
+      console.log("edudcaicon has created");
+      console.log(this.eduLoad);
+      window.location.reload();
+    })
+    this.eduLoad = new Educacion();
+  }
+
+  deleteEducation(id: number) {
+    this.educacionSvc.deleteEducation(id).subscribe(result => {
+      console.log(result);
+      window.location.reload();
     })
   }
 }
--- src/app/services/edu/educacion.service.ts
+++ src/app/services/edu/educacion.service.ts
@@ -1,3 +1,4 @@
+import { Educacion } from './../../models/educacion/educacion';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
@@ -7,13 +8,13 @@ import { Injectable } from '@angular/core';
 })
 export class EducacionService {
 
-  url: string = 'http://localhost:8080/education/see/all';
+  url: string = 'http://localhost:8080/education/';
 
   constructor(private _http: HttpClient) {
 
   }
 
-  getEduTodas(): Observable<any>{
+  getEduTodas(): Observable<any> {
     const httpOptions = {
       headers: new HttpHeaders({
 
@@ -22,6 +23,57 @@ export class EducacionService {
 
       })
     }
-    return this._http.get(this.url);
+    return this._http.get(this.url + "see/all");
+  }
+
+  getEducation(id: number): Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+
+      }),
+      params: new HttpParams({
+
+      })
+    }
+    return this._http.get(this.url + "see/" + id);
+  }
+
+  updateEducation(educacion: Educacion): Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+
+      }),
+      params: new HttpParams({
+
+      })
+    }
+    console.log(educacion);
+    return this._http.put<any>(this.url + "edit/" + educacion.eduId, educacion);
+  }
+
+  createdEducation(educacion: Educacion): Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+
+      }),
+      params: new HttpParams({
+
+      })
+    }
+    console.log(educacion);
+    return this._http.post(this.url + 'new', educacion)
+  }
+
+  deleteEducation(id: number): Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+
+      }),
+      params: new HttpParams({
+
+      })
+    }
+    console.log(id);
+    return this._http.delete(this.url + "delete/" + id)
   }
 }
